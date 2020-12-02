72baa377e3a91504b8e54142da367f135194f714
Denny Pearce <obsidian.y.dragon@gmail.com>
2020-12-02 23:31:58 +0200

    add new routes, add titles to them

--- index.js
+++ index.js
@@ -16,11 +16,21 @@ app.set("views", "views");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Main page",
+  });
 });
 
-app.get("/about", (req, res) => {
-  res.render("about");
+app.get("/add", (req, res) => {
+  res.render("add", {
+    title: "Add course",
+  });
+});
+
+app.get("/courses", (req, res) => {
+  res.render("courses", {
+    title: "Courses",
+  });
 });
 
 const PORT = process.env.PORT || 3000;
